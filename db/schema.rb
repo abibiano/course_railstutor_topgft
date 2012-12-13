b1cf74babac437c6aff39440626044a1ae345731
Alex Bibiano <alex@bibiano.es>
2012-12-13 15:23:02 +0100

    added comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210124000) do
+ActiveRecord::Schema.define(:version => 20121213133619) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "content"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "url"
