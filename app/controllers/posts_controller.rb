b9195974df8723c67bebd55814b3efb126820626
Alex Bibiano <alex@bibiano.es>
2013-01-10 14:40:28 +0100

    refactored with livesession comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,11 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all.sort_by { |post| post.vote_number }.reverse
-
-		respond_to do |format|
-			format.html # index.html.erb
-		end
 	end
 
 	def show
 		@post = Post.where(slug: params[:id]).first
 		@comment = Comment.new
-		respond_to do |format|
-			format.html # show.html.erb
-		end		
 	end
 
 	def new
