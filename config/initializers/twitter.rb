1452fab0c7936356a06dc586fd10ecc073e32a0f
Alex Bibiano <alex@bibiano.es>
2013-01-14 17:46:12 +0100

    twitter integration

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "xklcVzQzfbt4ujSxHvc5IA"
+  config.consumer_secret = "LJMeJsVSKObyKIetxOVDgkWBQPETJkNi0FvDRcM7TM"
+  config.oauth_token = "296693166-QLizKwZUjCt2MJOn1c7tvhEGANw5Ddmhff74wunp"
+  config.oauth_token_secret = "Jek6mI1A2JYrnamuYGv0v4Bwnp9wyR1XlokK4XHYE"
+end
