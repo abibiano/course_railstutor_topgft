b4bafb58c722582f15ec054c87555ed71b6dd350
Alex Bibiano <alex@bibiano.es>
2013-01-16 12:40:37 +0100

    debug

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,10 +9,9 @@ class Vote < ActiveRecord::Base
   
 
   def update_counter_cache
-  	puts "Hello, logs!"
+  	
     self.post.votes_count = Vote.where("direction = 'up' AND post_id = ?", self.post_id).count - Vote.where("direction = 'down' AND post_id = ?", self.post_id).count
-    puts Vote.where("direction = 'up' AND post_id = ?", self.post_id).count.to_s
-    puts self.post.votes_count.to_s
+        
     self.post.save
   end
 end
